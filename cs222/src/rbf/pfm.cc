ced3622d22ecee75910686e5096dcc7a110facc1
Tamir <tamirh@gmail.com>
2014-01-12 14:09:42 -0800

    Adding in basic file open/close code

--- cs222/src/rbf/pfm.cc
+++ cs222/src/rbf/pfm.cc
@@ -26,24 +26,63 @@ PagedFileManager::~PagedFileManager()
 
 RC PagedFileManager::CreateFile(const char *fileName)
 {
-    return rc::FEATURE_NOT_YET_IMPLEMENTED;
+    // Check if the file exists
+    FILE* file = fopen(fileName, "r");
+    if (file)
+    {
+        return rc::FILE_ALREADY_EXISTS;
+    }
+
+    fclose(file);
+    // NOTE: Possible concurrency issue, file may be created within this window
+    file = fopen(fileName, "w");
+
+    // TODO: Write any header data for the file
+    // TODO: Keep track that we have created this file
+
+    fclose(file);
+    return rc::OK;
 }
 
 
 RC PagedFileManager::DestroyFile(const char *fileName)
 {
-    return rc::FEATURE_NOT_YET_IMPLEMENTED;
+    if (remove(fileName) != 0)
+    {
+        return rc::FILE_COULD_NOT_DELETE;
+    }
+
+    // TODO: Keep track that we have deleted this file
+    return rc::OK;
 }
 
 
 RC PagedFileManager::OpenFile(const char *fileName, FileHandle &fileHandle)
 {
+    FILE* file = fopen(fileName, "r");
+    if (!file)
+    {
+        return rc::FILE_NOT_FOUND;
+    }
+
+    fclose(file);
+    file = fopen(fileName, "r+");
+    if (!file)
+    {
+        return rc::FILE_COULD_NOT_OPEN;
+    }
+
+    // TODO: Check fileHandle hasn't alreaedy been initialized
+    // TODO: Initialize fileHandle with file
     return rc::FEATURE_NOT_YET_IMPLEMENTED;
 }
 
 
 RC PagedFileManager::CloseFile(FileHandle &fileHandle)
 {
+    // TODO: Check fileHandle is properly initialized
+    // TODO: Write out data
+    // TODO: Uninitialize fileHandle
     return rc::FEATURE_NOT_YET_IMPLEMENTED;
 }
 
