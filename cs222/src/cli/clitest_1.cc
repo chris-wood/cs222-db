6f83e027466597ffe210f15196fdd74221604ae5
Tamir <tamirh@gmail.com>
2014-03-10 17:21:05 -0700

    More CLI tests

--- cs222/src/cli/clitest_1.cc
+++ cs222/src/cli/clitest_1.cc
@@ -21,9 +21,9 @@ void exec(bool print, string command, bool equal = true)
 		assert (cli->process(command) != SUCCESS);
 }
 
-void RepeatTest(const std::string& prefix, fn_test test, int numRetries)
+void RunTest(const std::string& testName, fn_test test, int numRetries)
 {
-	cout << prefix;
+	cout << "******************** CLI " << testName << "********************\n\n";
 
 	if (numRetries < 1)
 	{
@@ -35,10 +35,9 @@ void RepeatTest(const std::string& prefix, fn_test test, int numRetries)
 	}
 	
 	cout << "\n=========================================" << endl;
-	cout << "Repeating test " << (numRetries-1) << " times:";
+	cout << "Repeating test " << (numRetries-1) << " times...\n";
 	for (int i=1; i<numRetries; ++i)
 	{
-		cout << ".";
 		test(false);
 	}
 
@@ -57,11 +56,6 @@ void CreateDeleteTest(bool print)
 	exec(print, "create table bunch_of_nums n1 = int, n2 = int, n3 = int, n4 = int, n5 = int, n6 = real");
 	exec(print, "create table bunch_of_strings s1 = varchar(1), s2 = varchar(2), s3 = varchar(50), s4 = varchar(500)");
 	exec(print, "create table bunch_of_stuff foo = int, bar = varchar(50), biz = real, baz = real, qux = varchar(10), bad = int, good = real");
-	if (print)
-	{
-		exec(print, "print cli_columns");
-		exec(print, "print cli_tables");
-	}
 
 	// Delete all of the tables
 	if (print)
@@ -69,27 +63,59 @@ void CreateDeleteTest(bool print)
 	exec(print, "drop table bunch_of_nums");
 	exec(print, "drop table bunch_of_strings");
 	exec(print, "drop table bunch_of_stuff");
-	if (print)
-	{
-		exec(print, "print cli_columns");
-		exec(print, "print cli_tables");
-	}
 
 	// Recreate the tables we had before, but with different columns
 	if (print)
 		cout << "* Recreating tables with different columns..." << endl;
 	exec(print, "create table bunch_of_nums n1 = real, n2 = real, n3 = int, n4 = int, n5 = int, n6 = int");
 	exec(print, "create table bunch_of_strings s1 = varchar(11), s2 = varchar(100), s3 = varchar(4), s4 = varchar(10)");
-	exec(print, "create table bunch_of_stuff foo = varchar(10), bar = real, biz = int, baz = int, qux = varchar(10), bad = int, good = int");
-	if (print)
-	{
-		exec(print, "print cli_columns");
-		exec(print, "print cli_tables");
-	}
+	exec(print, "create table bunch_of_stuff foo = varchar(10), bar = real, biz = int, baz = int, qux = varchar(10), good = int");
 
 	// Create a bunch of indexes
+	if (print)
+		cout << "* Creating indexes on all of the columns" << endl;
+	exec(print, "create index n1 on bunch_of_nums");
+	exec(print, "create index n1 on bunch_of_nums", false);
+	exec(print, "create index nX on bunch_of_nums", false);
+	exec(print, "create index n2 on bunch_of_nums");
+	exec(print, "create index n3 on bunch_of_nums");
+	exec(print, "create index n4 on bunch_of_nums");
+	exec(print, "create index n5 on bunch_of_nums");
+	exec(print, "create index n6 on bunch_of_nums");
+	exec(print, "create index n6 on bunch_of_nums", false);
+	exec(print, "create index s1 on bunch_of_strings");
+	exec(print, "create index s2 on bunch_of_strings");
+	exec(print, "create index s3 on bunch_of_strings");
+	exec(print, "create index s4 on bunch_of_strings");
+	exec(print, "create index foo on bunch_of_stuff");
+	exec(print, "create index bar on bunch_of_stuff");
+	exec(print, "create index biz on bunch_of_stuff");
+	exec(print, "create index baz on bunch_of_stuff");
+	exec(print, "create index qux on bunch_of_stuff");
+	exec(print, "create index bad on bunch_of_stuff", false);
+	exec(print, "create index good on bunch_of_stuff");
 
 	// Delete SOME of the indexes
+	if (print)
+		cout << "* Deleting indexes on all but one column from each table" << endl;
+	exec(print, "drop index n2 on bunch_of_nums");
+	exec(print, "drop index n3 on bunch_of_nums");
+	exec(print, "drop index n4 on bunch_of_nums");
+	exec(print, "drop index n5 on bunch_of_nums");
+	exec(print, "drop index n6 on bunch_of_nums");
+	exec(print, "drop index nX on bunch_of_nums", false);
+	exec(print, "drop index s2 on bunch_of_strings");
+	exec(print, "drop index s3 on bunch_of_strings");
+	exec(print, "drop index s4 on bunch_of_strings");
+	exec(print, "drop index foo on bunch_of_stuff");
+	exec(print, "drop index bar on bunch_of_stuff");
+	exec(print, "drop index biz on bunch_of_stuff");
+	exec(print, "drop index baz on bunch_of_stuff");
+	exec(print, "drop index qux on bunch_of_stuff");
+	exec(print, "drop index qux on bunch_of_stuff", false);
+	exec(print, "drop index bad on bunch_of_stuff", false);
+
+	// At this point, we should only be left with bunch_of_nums.n1 bunch_of_strings.s1, and bunch_of_stuff.good
 
 	// Delete all of the tables again
 	if (print)
@@ -97,16 +123,56 @@ void CreateDeleteTest(bool print)
 	exec(print, "drop table bunch_of_nums");
 	exec(print, "drop table bunch_of_strings");
 	exec(print, "drop table bunch_of_stuff");
+
+	// Recreate tables with just a single column (the column we didn't delete the index on)
 	if (print)
-	{
-		exec(print, "print cli_columns");
-		exec(print, "print cli_tables");
-	}
+		cout << "* Recreating tables with reduced columns..." << endl;
+	exec(print, "create table bunch_of_nums n1 = real");
+	exec(print, "create table bunch_of_strings s1 = varchar(11)");
+	exec(print, "create table bunch_of_stuff good = int");
+
+	// Make sure the indexes didn't linger
+	if (print)
+		cout << "* Checking to see indexes didn't survive..." << endl;
+	exec(print, "create index n1 on bunch_of_nums");
+	exec(print, "create index s1 on bunch_of_strings");
+	exec(print, "create index good on bunch_of_stuff");
+
+	// Finally clean up and delete everything
+	exec(print, "drop table bunch_of_nums");
+	exec(print, "drop table bunch_of_strings");
+	exec(print, "drop table bunch_of_stuff");
+
+	exec(print, "drop table bunch_of_nums", false);
+	exec(print, "drop table bunch_of_strings", false);
+	exec(print, "drop table bunch_of_stuff", false);
 }
 
-void CreateDeleteTest(int numRetries)
+void VeryLargeIndexTest(bool print)
 {
-	RepeatTest("*********** CLI BasicTest begins ******************", CreateDeleteTest, numRetries);
+	if (print)
+	{
+		cout << "* We will fill up a table with a lot of indexed data and then filter it down to a small result" << endl;
+		cout << "* Creating initial tables..." << endl;
+	}
+
+	// Create our table and indexes
+	exec(print, "create table full_table i = int, r = real, s = varchar(100)");
+	exec(print, "create index i on full_table");
+	exec(print, "create index r on full_table");
+	exec(print, "create index s on full_table");
+
+	// TODO: Test current values
+
+	// TODO: Drop indexes and start adding more values
+
+	// TODO: Re-add indexes, and they should be on all of the new values
+
+	// TODO: Test current values
+
+	// Finally clean up by deleting tables
+	exec(print, "drop table full_table");
+	exec(print, "drop table full_table", false);
 }
 
 void cleanup()
@@ -118,7 +184,10 @@ void cleanup()
 	remove("cli_indexes");
 	remove("cli_tables");
 
-	remove("tbl_employee");
+	remove("bunch_of_nums");
+	remove("bunch_of_strings");
+	remove("bunch_of_stuff");
+	remove("full_table");
 }
 
 int main()
@@ -126,7 +195,8 @@ int main()
 	cleanup();
 	cli = CLI::Instance();
 
-	CreateDeleteTest(64);
+	RunTest("CreateDelete", CreateDeleteTest, 2);
+	RunTest("VeryLargeIndex", VeryLargeIndexTest, 2);
 
 	cleanup();
 	return 0;
